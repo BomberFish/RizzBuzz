6f34059d7d7066688ab1c911ba0ab21aa5d7b2ca
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-10-22 22:21:53 -0400

    implement in bash just for you coolelectronics :3

---
+++ rizzbuzz.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for i in $(seq 0 100);
+do
+    if ((i % 3 == 0)) && ((i % 5 == 0))
+    then
+        echo "RizzBuzz"
+    elif ((i % 3 == 0))
+    then
+        echo "Rizz"
+    elif ((i % 5 == 0))
+    then
+        echo "Buzz"
+    else
+        echo $i
+    fi
+done
